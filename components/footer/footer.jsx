5c471ff745721fecba829ed020dece35b1c1b9e0
Cassandra Tang <cassandra.tang@hotmail.com>
2025-04-05 20:48:22 +0100

    added new packages

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export default function FooterComponent() {
   return (
     <Footer>
-      <h2>Last updated 05.04.2025</h2>
+      <h2>Last updated 05/04/2025</h2>
       <h2>
         Say hey @{" "}
         <a href="https://cassandratang.com">cassandratang.com</a>
